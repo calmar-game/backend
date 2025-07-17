f9772d2c56b57c4bf77a58a9afa774c8574d15db
kusainovv <kusainovratmir89@gmail.com>
2025-07-17 22:18:08 +0400

    Update JWT token expiration in UserService from 2 minutes to 7 days, with a note to remove the TODO comment.

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -210,7 +210,7 @@ export class UserService {
   
     const gameToken = await this.jwtService.signAsync(payload, {
       secret: process.env.JWT_ACCESS_SECRET,
-      expiresIn: '2m' // TODO: Fix that
+      expiresIn: '7d' // TODO: Fix that
     });
   
     return gameToken;
